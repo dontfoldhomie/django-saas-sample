5d293c4171f69f939ac44541e636e5f81176a1e0
Ezekiel Arcelona <ezekielarcelona@gmail.com>
2024-07-02 11:49:56 -0700

    updated views

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,8 +9,8 @@ this_dir = pathlib.Path(__file__).resolve().parent
 def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
-    my_title = "My Page"
     html_template = "home.html"
+    my_title = "My Page"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
